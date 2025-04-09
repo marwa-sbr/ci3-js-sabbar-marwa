5dfcfe014d085af1ab7dc76c53c6b0544268be8e
marwa-sbr <m_sabbar@stu-digital-campus.fr>
2025-04-09 12:01:38 +0200

    Add files via upload

---
+++ lvl1-ex2.js
@@ -0,0 +1,7 @@
+
+let nom = prompt ("Quel est ton nom ?");
+
+alert("Bonjour," + nom + "!");
+
+    console.log ("Bonjour," + nom + "!"); 
+
---
+++ lvl1-ex3.js
@@ -0,0 +1,10 @@
+function sum(a,b) {
+    return a + b;
+}
+
+let nombre = prompt ("Entrez vos nombres", 10,13);
+
+let result = sum (10,12);
+alert (result);
+
+
